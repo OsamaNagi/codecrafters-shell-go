db7b83a2e8a8be284063fee3ca1c84865b25e405
OsamaNagi <osamanagiworks@gmail.com>
2025-02-07 22:39:11 +0200

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var _ = fmt.Fprint
@@ -14,11 +15,17 @@ func main() {
 
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
+		command = strings.TrimSpace(command)
+
+		if command == "exit 0" {
+			os.Exit(0)
+		}
+
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading command", err)
 			os.Exit(1)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		fmt.Println(command + ": command not found")
 	}
 }
